a29125af5b8b8c97f321959f51ba4423fc385470
Brady Law <bradyl16@gmail.com>
2025-09-05 15:05:19 -0700

    Revert "drivers: cc33xx: update to 1.0.0.8"
    This reverts commit 3f6fedd7142e0535de1eabbdee81bded03307502.

--- drivers/net/wireless/ti/cc33xx/ps.c
+++ drivers/net/wireless/ti/cc33xx/ps.c
@@ -20,6 +20,7 @@ int cc33xx_ps_set_mode(struct cc33xx *cc, struct cc33xx_vif *wlvif,
 		if (ret < 0)
 			return ret;
 
+		set_bit(WLVIF_FLAG_IN_PS, &wlvif->flags);
 		break;
 
 	case STATION_ACTIVE_MODE:
@@ -27,6 +28,7 @@ int cc33xx_ps_set_mode(struct cc33xx *cc, struct cc33xx_vif *wlvif,
 		if (ret < 0)
 			return ret;
 
+		clear_bit(WLVIF_FLAG_IN_PS, &wlvif->flags);
 		break;
 
 	default:
